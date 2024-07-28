d2561b7420ac4b8777bc5462f4bbf84cd8f277fe
Anthony <139293883+caterva21@users.noreply.github.com>
2024-07-28 11:26:20 -0300

    I add a data types file

---
+++ JavaScript/basic_structure/definitions/typeOfData.js
@@ -0,0 +1,75 @@
+/*brief: This file is about the type of data in JavaScript.
+ *Observation: Most data types in JavaScript are objects
+ *date: 28/07/2024
+*version: 1.0*/
+
+
+// type of date string
+let name = "John";
+alert(name); 
+console.log(typeof name);   // output: string
+
+// type of date number
+let age = 25;
+alert(age);
+console.log(typeof age);  // output: number
+
+// type of date boolean
+let isMarried = false;
+alert(isMarried);
+console.log(typeof isMarried); // output: boolean
+
+// type of date null
+let car = null;
+alert(car);
+console.log(typeof car);  // output: object
+
+// type of date undefined
+let test;
+alert(test);
+console.log(typeof test);  // output: undefined  
+
+// type of date symbol
+let id = Symbol("id");
+alert(id.description);
+console.log(typeof id);  // output: symbol
+
+// type of date array
+let fruits = ["Apple", "Orange", "Plum"];
+alert(fruits);
+console.log(typeof fruits);  // output: object
+
+// type of date matrix
+let matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
+alert(matrix);
+console.log(typeof matrix);  // output: object
+
+// type of date function
+function sayHi() {
+  alert("Hello");
+}
+alert(sayHi);
+console.log(typeof sayHi);  // output: function
+
+// type of date set
+let set = new Set([1, 2, 3, 4, 5]);
+alert(set);
+console.log(typeof set);   // output: object
+
+// type of date map
+let map = new Map();
+map.set("1", "str1");
+map.set(1, "num1");
+alert(map);
+console.log(typeof map);  // output: object
+
+// type of date object
+let user = {name: "John", lastName: "Smith"};
+alert(user);
+console.log(user); 
+console.log(typeof user);  // output: object
+
+// type of date date
+let date = new Date();
+alert(date);
+console.log(typeof date);  // output: object
---
+++ JavaScript/basic_structure/loops/progressiveCount.js
@@ -0,0 +1,12 @@
+/*brief: Asks the user for a maximum number and performs a progressive count
+ *date: 28/07/2024
+ *version: 1.0*/
+
+
+let maxNumber = prompt("Enter a value: ");
+let account = 0;
+while(account <= maxNumber){
+    alert(account);
+    console.log(account);
+    account++;
+}
