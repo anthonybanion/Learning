e7bd14a9804ef7d818bd560a4bc7796d67260a65
anthony-xubuntu <abanon@fi.uba.ar>
2024-11-09 22:47:24 -0300

    agregando algunos cambios

---
+++ Java/basic/Objects.java
@@ -0,0 +1,15 @@
+
+
+public class Objects{
+    String name;
+
+
+public static void main(String[] args){
+    Objects uno = new Objects();
+    uno.name= "peru";
+    
+    System.out.println(uno);  //Objects@1dbd16a6 nos da la direccion de memoria
+    System.out.println(uno.name);
+}
+
+}
