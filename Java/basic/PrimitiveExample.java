48d7658067ebb14057134b0541022ebf8c18fd5f
Anthony <abanon@fi.uba.ar>
2025-02-09 16:40:15 -0300

    chapter2

---
+++ Java/basic/PrimitiveExample.java
@@ -0,0 +1,13 @@
+public class PrimitiveExample {
+    public static void main(String[] args) {
+        int number = 100;
+        double decimal = 99.99;
+        char letter = 'A';
+        boolean isTrue = true;
+
+        System.out.println("Integer: " + number);
+        System.out.println("Decimal: " + decimal);
+        System.out.println("Character: " + letter);
+        System.out.println("Boolean: " + isTrue);
+    }
+}
