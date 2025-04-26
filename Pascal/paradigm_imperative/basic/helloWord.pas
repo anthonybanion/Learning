8fe3b1ab8b67c741953f7ce22709ad457edaf6c0
anthony-xu <caterva17@hotmail.com>
2025-04-26 10:50:44 -0300

    [Learning][Pascal]=> General structure in Pascal language was created, Closes #105

--- Pascal/paradigm_imperative/basic/helloWord.pas
+++ Pascal/paradigm_imperative/basic/helloWord.pas
@@ -1,5 +1,18 @@
-program helloWorld;
+{*
+  
+  Description: This is a simple Pascal program that prints "Hello, World!" to the console.
+  
+  File: helloWord.pas
+  Author: Anthony Bañon
+  Created: 2025-04-26
+  Last Updated: 2025-04-26
+*}
 
+
+
+program helloWorld;
 begin
     writeln('Hello, World!');
+    writeln('Hello, World!'); 
+
 end.
