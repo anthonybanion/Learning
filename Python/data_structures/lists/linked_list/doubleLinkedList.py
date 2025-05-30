4871a16f28f0d5bda4d49d23570eab43886062d4
anthony-xu <caterva17@hotmail.com>
2025-05-29 22:20:07 -0300

    [Learning][Python]=> Add double Linked List

--- Python/data_structures/lists/linked_list/doubleLinkedList.py
+++ Python/data_structures/lists/linked_list/doubleLinkedList.py
@@ -35,31 +35,63 @@ class LinkedList:
                 current = current.next
             print("None")
 
+    # def delete(self, data):
+    #     current = self.head
+    #     before = None      
+
+    #     while current and current.data != data:
+    #         before = current
+    #         current = current.next
+    #     if current is None:
+    #         print("Element not found")
+    #         return False
+    #     if before is None:
+    #         self.head = current.next
+    #     else:
+    #         before.next = current.next
+    #         current.former = before
+    #     self.size -= 1
+    #     return True
+
     def delete(self, data):
         current = self.head
-        before = None
-        
-
         while current and current.data != data:
-            before = current
             current = current.next
-        if current is None:
+        if not current:
             print("Element not found")
-            return False
-        if before is None:
-            self.head = current.next
+            return False 
+        if not current.former:
+                self.head = current.next
         else:
-            before.next = current.next
-        self.size -= 1
-        return True
-    
+            current.former.next = current.next
+            return True  
+        if current.next:
+            current.next.former = current.former
+
+
+    def search(self, data):
+        current = self.head
+        while current:
+            if current.data == data:
+                print("Element found")
+                return current
+            current = current.next
+        print("Element not found")
+        return None
+
+
+
 
 __main__ = "__main__"
-simpleList = LinkedList() 
-simpleList.append(4)
-simpleList.append(2)
-simpleList.append(1)
-simpleList.append(3)
+doubleList = LinkedList() 
+doubleList.append(4)
+doubleList.append(2)
+doubleList.append(1)
+doubleList.append(3)
 
-LinkedList.show(simpleList)
-print("Size of the list: ", simpleList.size)
+LinkedList.show(doubleList)
+print("Size of the list: ", doubleList.size)
+
+LinkedList.delete(doubleList, 2)
+LinkedList.show(doubleList)
+print(doubleList.search(4).next)
