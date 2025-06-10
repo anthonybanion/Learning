a37a9221e491bbc9c7cf98fc203c768792ac6c1f
anthony-xu <caterva17@hotmail.com>
2025-06-10 11:19:59 -0300

    [Learning][ Php]=> Add old file, See #136

---
+++ Php/security/session_management/sessionDestroy.php
@@ -0,0 +1,10 @@
+<?php
+/* @breif: start a session and destroy it
+ * @details: linked with session_start.php
+ * @file: session_start.php
+ * @date: 05/04/2024
+ * version: 1.0*/
+
+session_start();
+session_destroy()
+?>
